60340ef04ae588b3b453cecd378ee91f4f17abcb
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-10-17 14:27:43 +0300

    skills done

--- src/Component/Cards/Skills.jsx
+++ src/Component/Cards/Skills.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { VscDebugBreakpointLog } from "react-icons/vsc";
 import * as skills from "../../data/skills.js";
 import dataCSS from "../../data/dataCSS.js";
+import SkillsSection from "./SkillsSection.jsx";
 function Skills({ slideIndex, styles }) {
   return (
     <div
@@ -9,90 +10,27 @@ function Skills({ slideIndex, styles }) {
       style={{ translate: `${-100 * slideIndex}%` }}
     >
       <div className={styles.skills_container}>
-        <div className={styles.skills_sections}>
-          <h2
-            style={{
-              background: dataCSS[dataCSS.length - 1],
-              backgroundClip: "text",
-              color: "transparent",
-            }}
-          >
-            FrontEnd
-          </h2>
-          <div className={styles.skills}>
-            <ul>
-              {skills.FrontEnd.map((f, i) => (
-                <li
-                  key={f}
-                  style={{
-                    background: dataCSS[i + 2],
-                    backgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  <VscDebugBreakpointLog className={styles.skills_icon} />
-                  {f}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className={styles.skills_sections}>
-          <h2
-            style={{
-              background: dataCSS[dataCSS.length - 2],
-              backgroundClip: "text",
-              color: "transparent",
-            }}
-          >
-            BackEnd
-          </h2>
-          <div className={styles.skills}>
-            <ul>
-              {skills.BackEnd.map((f, i) => (
-                <li
-                  key={f}
-                  style={{
-                    background: dataCSS[i],
-                    backgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  <VscDebugBreakpointLog className={styles.skills_icon} />
-                  {f}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className={styles.skills_sections}>
-          <h2
-            style={{
-              background: dataCSS[dataCSS.length - 3],
-              backgroundClip: "text",
-              color: "transparent",
-            }}
-          >
-            Others
-          </h2>
-          <div className={styles.skills}>
-            <ul>
-              {skills.Other.map((f, i) => (
-                <li
-                  key={f}
-                  style={{
-                    background: dataCSS[i],
-                    backgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  <VscDebugBreakpointLog className={styles.skills_icon} />
-                  {f}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <SkillsSection
+          data={skills.FrontEnd}
+          styles={styles}
+          dataCSS={dataCSS}
+          startIndex={0}
+          name={"FrontEnd"}
+        />
+        <SkillsSection
+          data={skills.BackEnd}
+          styles={styles}
+          dataCSS={dataCSS}
+          startIndex={5}
+          name={"BackEnd"}
+        />
+        <SkillsSection
+          data={skills.Other}
+          styles={styles}
+          dataCSS={dataCSS}
+          startIndex={2}
+          name={"Other"}
+        />
       </div>
     </div>
   );
