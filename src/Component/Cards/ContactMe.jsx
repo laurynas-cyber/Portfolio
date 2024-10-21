105b1431ddfc9b0a5f85e08cdde13084b712d49b
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-10-21 15:28:56 +0300

    backEnd

--- src/Component/Cards/ContactMe.jsx
+++ src/Component/Cards/ContactMe.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function ContactMe({ slideIndex, styles }) {
   const [form, setForm] = useState({
@@ -6,9 +7,23 @@ export default function ContactMe({ slideIndex, styles }) {
     email: "",
     message: "",
   });
-
+  const [responseMessage, setResponseMessage] = useState("");
   const handleForm = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/send-email",
+        form
+      );
+      setResponseMessage(response.data);
+    } catch (error) {
+      setResponseMessage("Error sending message. Please try again.");
+    }
   };
   return (
     <div
@@ -17,7 +32,7 @@ export default function ContactMe({ slideIndex, styles }) {
     >
       <div className={styles.contact_container}>
         <h2>Write me a message</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className={styles.contact_wrap}>
             <span className={styles.sub}>Subject</span>
             <input
@@ -45,7 +60,9 @@ export default function ContactMe({ slideIndex, styles }) {
               id=""
             ></textarea>
           </div>
+          <button type="submit">Submit</button>
         </form>
+        {responseMessage && <p>{responseMessage}</p>}
       </div>
     </div>
   );
