e2973a8ff2bf97b89f6b5d24fa2017bf754586d9
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-10-22 13:13:46 +0300

    contacte me validate

--- portfolio/src/Component/Cards/ContactMe.jsx
+++ portfolio/src/Component/Cards/ContactMe.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import useRegister from "../Validations/useRegister";
 
 export default function ContactMe({ slideIndex, styles }) {
   const [form, setForm] = useState({
@@ -8,21 +9,30 @@ export default function ContactMe({ slideIndex, styles }) {
     message: "",
   });
   const [responseMessage, setResponseMessage] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const { errors, validate, setServerErrors } = useRegister();
   const handleForm = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setButtonDisabled(true);
+    if (!validate(form)) {
+      setButtonDisabled(false);
+      setResponseMessage(errors.email);
+      return;
+    }
     try {
       const response = await axios.post(
         "http://localhost:5000/send-email",
         form
       );
       setResponseMessage(response.data);
+      setButtonDisabled(false);
     } catch (error) {
       setResponseMessage("Error sending message. Please try again.");
+      setButtonDisabled(false);
     }
   };
   return (
@@ -31,7 +41,17 @@ export default function ContactMe({ slideIndex, styles }) {
       style={{ translate: `${-100 * slideIndex}%` }}
     >
       <div className={styles.contact_container}>
-        <h2>Write me a message</h2>
+        <h2>Write me a message </h2>
+        <div
+          className={
+            responseMessage
+              ? styles.message_card + " " + styles.got_msg
+              : styles.message_card
+          }
+        >
+          {responseMessage}
+        </div>
+
         <form onSubmit={handleSubmit}>
           <div className={styles.contact_wrap}>
             <span className={styles.sub}>Subject</span>
@@ -60,11 +80,14 @@ export default function ContactMe({ slideIndex, styles }) {
               id=""
             ></textarea>
           </div>
-          <button type="submit" className={styles.submit_btn}>
+          <button
+            disabled={buttonDisabled}
+            type="submit"
+            className={styles.submit_btn}
+          >
             Submit
           </button>
         </form>
-        {responseMessage && <p>{responseMessage}</p>}
       </div>
     </div>
   );
---
+++ portfolio/src/Component/Validations/useRegister.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function useRegister() {
+  const [errors, setErrors] = useState({});
+
+  const setServerErrors = (err) => {
+    setErrors(err);
+  };
+
+  const validate = (form) => {
+    const errorsBag = {};
+
+
+
+    if (!/\S+@\S+\.\S+/.test(form.email)) {
+      errorsBag.email = "Email format is invalid";
+    }
+
+    // return true; // laikinai
+
+    if (Object.keys(errorsBag).length === 0) {
+      setErrors({});
+      return true;
+    } //tikrinam kokie yra key yra tam objekte. taip galima tikrinti ar jis tuscias ar ne.
+
+    setErrors(errorsBag);
+    return false;
+  };
+
+  return { errors, validate, setServerErrors };
+}
+
+export default useRegister;
