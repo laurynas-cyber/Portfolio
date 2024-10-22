78164173ce5d1ecfc11d62a2496b7753bcde4c41
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-10-22 11:44:57 +0300

    links new QR

--- portfolio/src/Component/Cards/Links.jsx
+++ portfolio/src/Component/Cards/Links.jsx
@@ -1,11 +1,11 @@
-import GitQR from "../../assets/githubV.png";
-import LinQR from "../../assets/linkedinV.png";
+import GitQR from "../../assets/Github2.png";
+import LinQR from "../../assets/LinkedIn2.png";
 
 function Links({ slideIndex, styles }) {
   return (
     <div
       className={styles.portfolio_content}
-      style={{ translate: `${-100 * slideIndex}%` }}
+      style={{ translate: `${-100 * slideIndex}%`, backgroundColor: "#263547" }}
     >
       <div className={styles.links_container}>
         <div className={styles.links_QR_container}>
